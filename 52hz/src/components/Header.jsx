0645d1aaf9ed2bfb6d6de9a8e85b5c94632af15f
trinityprime <80157949+trinityprime@users.noreply.github.com>
2023-08-23 23:34:44 +0800

    head

---
+++ 52hz/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../index.css'
+import headerback from '../assets/headerback.jpg'
+
+const Header = () => {
+    return (
+        <div>
+            <body>
+                <div className='relative'>
+                    <img className='opacity-40 scale-50' src={headerback} />
+                    <div className='absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                        <h1 className='text-[white] text-9xl '>52 Blue</h1>
+                        <h2 className='text-[white] text-2xl'>The loneliest whale in the world.</h2>
+                    </div>
+                </div>
+            </body>
+        </div>
+    )
+}
+
+export default Header
