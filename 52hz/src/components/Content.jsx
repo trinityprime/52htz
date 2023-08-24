eef3ffb75f7ba9f2ce30c3ab24e8a63092085ff4
trinityprime <80157949+trinityprime@users.noreply.github.com>
2023-08-25 00:52:04 +0800

    audio player

--- 52hz/src/components/Content.jsx
+++ 52hz/src/components/Content.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/index.css'
+import useSound from 'use-sound'
+import whalecall from '../assests/whalecall.wav'
+import { CgPlayButtonO, CgPlayPauseO } from 'react-icons/cg'
 
 const Content = () => {
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    const [playBoop, { pause }] = useSound(whalecall, {
+        onplay: () => setIsPlaying(true),
+        onend: () => setIsPlaying(false),
+    });
+
+    const togglePlay = () => {
+        if (isPlaying) {
+            pause(); 
+        } else {
+            playBoop();
+        }
+        setIsPlaying(!isPlaying);
+
+
+    }
+
     return (
         <>
-            <div className='text-white w-3/5 m-auto box-border text-left'>
-                <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+            <div className='w-full bg-white py-16'>
+
+                <div className='max-w-[1000px] mx-auto grid md:grid-cols-2'>
+                    <div className='text-6xl mx-auto '>
+                        <p>ONE OF <br></br>A KIND</p>
+                    </div>
+
+                    <div className='text-2xl -ml-20 pt-3' >
+                        <p>With blue whales usually having a vocalization range of 10-39Hz, 52 Blue's call of 52Hz is much higher in frequency, shorter and more frequent.</p>
+                    </div>
+                </div>
+
+                <div className='max-w-[2000px] flex py-8 pl-10 justify-center'>
+                    <button onClick={togglePlay}>
+                        {isPlaying ? <CgPlayPauseO size={50} /> : <CgPlayButtonO size={50} />}  {/* ? = if , : = else*/}
+                    </button>
+                    <p className='text-2xl p-4'>This signal was recorded in the northeast Pacific.<br></br>It has been sped up 10x, raising the pitch to 520 Hz.</p>
+                </div>
+
             </div>
         </>
     )
