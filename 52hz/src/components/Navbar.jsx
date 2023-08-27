f8924cdd21532b7e171fa1cbe223ed96da186560
trinityprime <80157949+trinityprime@users.noreply.github.com>
2023-08-27 23:34:08 +0800

    navbar edit

--- 52hz/src/components/Navbar.jsx
+++ 52hz/src/components/Navbar.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 import '../styles/index.css'
 
 const Navbar = () => {
+
     return (
         <>
             <div className='flex justify-center h-24 mx-0 px-4 text-xl text-white'>
-                <ul className='flex'>
-                    <li className='p-4'>About</li>
-                    <li className='p-4'>Contact</li>
-                    <li className='p-4'>More</li>
+                <ul className='flex p-4'>
+                    <li className='p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50'>
+                        <Link activeClass='active' smooth spy to="about">
+                            ABOUT
+                        </Link>
+                    </li>
+                    <li className='p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50'>
+                        <Link activeClass='active' smooth spy to="Contact">
+                            CONTACT
+                        </Link>
+                    </li>
+                    <li className='p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50'>
+                        <Link activeClass='active' smooth spy to="More">
+                            MORE
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </>
