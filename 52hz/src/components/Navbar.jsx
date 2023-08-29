f7748a2ba3dd9dad09013f5af6e55646bc4eaab4
trinityprime <80157949+trinityprime@users.noreply.github.com>
2023-08-29 18:50:56 +0800

    navbar scroll fade in out

--- 52hz/src/components/Navbar.jsx
+++ 52hz/src/components/Navbar.jsx
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import "../styles/index.css";
 
 const Navbar = () => {
+  const [navColor, setNavColor] = useState(false);
+
+  const changeColor = () => {
+    if (window.scrollY >= 1000) {
+      setNavColor(true);
+    } else {
+      setNavColor(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+    return () => {
+      window.removeEventListener("scroll", changeColor);
+    };
+  }, []);
+
   return (
-    <>
-      <header>
-        <div className="flex justify-center h-24 mx-0 px-4 text-xl text-white">
-          <ul className="flex p-4">
-            <li className="p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50">
-              <Link activeClass="active" smooth spy to="about">
-                ABOUT
-              </Link>
-            </li>
-            <li className="p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50">
-              <Link activeClass="active" smooth spy to="Contact">
-                CONTACT
-              </Link>
-            </li>
-            <li className="p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50">
-              <Link activeClass="active" smooth spy to="More">
-                MORE
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </header>
-    </>
+    <div className={navColor ? "header-bg" : "header"}>
+      <div className="flex justify-center h-24 mx-0 px-4 text-xl text-white">
+        <ul className="flex p-4">
+          <li className="p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50">
+            <Link activeClass="active" smooth spy to="about">
+              ABOUT
+            </Link>
+          </li>
+          <li className="p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50">
+            <Link activeClass="active" smooth spy to="Contact">
+              CONTACT
+            </Link>
+          </li>
+          <li className="p-4 hover:text-blue-200 hover:cursor-pointer hover:underline transition ease-in-out delay-50">
+            <Link activeClass="active" smooth spy to="More">
+              MORE
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
